161d728651ed97942b23a398dc56e088eb4d6864
Prathamesh Kulkarni <prathameshvk50@gmail.com>
2022-08-05 12:24:35 +0530

    Added previewer component

--- src/components/Previewer.jsx
+++ src/components/Previewer.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Previewer() {
-  return <div>Previewer</div>;
+  const navigate = useNavigate();
+  const resumeData = useSelector((state) => state.resume.resumeData);
+  return (
+    <div>
+      <div>
+        <button onClick={() => navigate("/edit")}>go back</button>
+      </div>
+      <div>
+        <h1>Previewer</h1>
+        <div>
+          <div>{resumeData.personalDetails.name}</div>
+          <div>{resumeData.personalDetails.birthDate}</div>
+          <div>{resumeData.personalDetails.contactDetails.email}</div>
+          <div>{resumeData.personalDetails.contactDetails.phone}</div>
+        </div>
+        <div>
+          {resumeData.educationDetails.map((item, key) => {
+            return <div key={key}>{item.graduationLevel}</div>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Previewer;
